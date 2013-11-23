eb3a13e71096a66dcd649cda0e9df2e68d25b2aa
twardakm <twardakm@gmail.com>
2013-11-23 19:20:15 +0100

    Odczytywanie wielkości obrazka

--- plik.c
+++ plik.c
@@ -16,7 +16,7 @@ int odczytaj_plik(element *lista)
     if((plik = fopen(nazwa_pliku, "r")) == NULL)
     {
         perror("Nie udało się otworzyć podanego pliku\n");
-        return 1;
+        return FILE_OPEN_ERR;
     }
     else
         printf("OK\n");
@@ -28,24 +28,31 @@ int odczytaj_plik(element *lista)
     temp->img->nazwa_pliku = (char *)malloc(sizeof(char) * strlen(nazwa_pliku));
     strcpy(temp->img->nazwa_pliku, nazwa_pliku);
 
-    if(odczytaj_rodzaj_obrazka(plik, temp->img))
+    if((plik = odczytaj_rodzaj_obrazka(plik, temp->img)) == NULL)
     {
         printf("Błąd odczytu z pliku\n");
         fclose(plik);
         free(nazwa_pliku);
-        return 1;
+        return FILE_READ_ERR;
+    }
+    if((plik = odczytaj_wielkosc_obrazka(plik, temp->img)) == NULL)
+    {
+        printf("Błąd odczytu z pliku\n");
+        fclose(plik);
+        free(nazwa_pliku);
+        return FILE_READ_ERR;
     }
 
     fclose(plik);
     free(nazwa_pliku);
 }
 
-int odczytaj_rodzaj_obrazka(FILE *plik, obraz *img)
+FILE * odczytaj_rodzaj_obrazka(FILE *plik, obraz *img)
 {
-    if (sprawdz_czy_komentarz(plik))
+    if ((plik = sprawdz_czy_komentarz(plik)) == NULL)
     {
         if(_DEBUG) printf("Nie udało się sprawdzanie komentarza\n");
-        return 1;
+        return NULL;
     }
     char c;
     //odczytanie litery 'P'
@@ -54,7 +61,7 @@ int odczytaj_rodzaj_obrazka(FILE *plik, obraz *img)
     {
         printf("Błędny nagłówek pliku!\n");
         if (_DEBUG) printf("(1) Odczytano: %c\n", c);
-        return 1;
+        return NULL;
     }
     if (_DEBUG) printf("(1) Odczytano: %c\n", c);
     //odczytanie rodzaju
@@ -63,13 +70,42 @@ int odczytaj_rodzaj_obrazka(FILE *plik, obraz *img)
     {
         printf("Błędny nagłówek pliku!\n");
         if (_DEBUG) printf("(2) Odczytano: %d\n", img->rodzaj);
-        return 1;
+        return NULL;
     }
     if (_DEBUG) printf("(2) Odczytano: %d\n", img->rodzaj);
-    return 0;
+
+    while(fgetc(plik) != '\n') {} // konieczne, bo inaczej wywala śmieci
+    return plik;
 }
 
-int sprawdz_czy_komentarz(FILE *plik)
+FILE * odczytaj_wielkosc_obrazka(FILE *plik, obraz *img)
+{
+    if ((plik = sprawdz_czy_komentarz(plik)) == NULL)
+    {
+        if(_DEBUG) printf("Nie udało się sprawdzanie komentarza\n");
+        return NULL;
+    }
+    fscanf(plik,"%d",&img->width);
+    if(img->width <= 0)
+    {
+        printf("Błędny nagłówek pliku!\n");
+        if (_DEBUG) printf("(3) Odczytano: %d\n", img->width);
+        return NULL;
+    }
+    if (_DEBUG) printf("(3) Odczytano: %d\n", img->width);
+    fscanf(plik, "%d", &img->height);
+    if(img->width <= 0)
+    {
+        printf("Błędny nagłówek pliku!\n");
+        if (_DEBUG) printf("(4) Odczytano: %d\n", img->height);
+        return NULL;
+    }
+    if (_DEBUG) printf("(4) Odczytano: %d\n", img->height);
+    while(fgetc(plik) != '\n') {}
+    return plik;
+}
+
+FILE * sprawdz_czy_komentarz(FILE *plik)
 {
     char c = fgetc(plik);
     while (c == '#')
@@ -81,16 +117,16 @@ int sprawdz_czy_komentarz(FILE *plik)
     if (!fseek(plik, ftell(plik) - 1, SEEK_SET) && c != EOF)
     {
         if (_DEBUG) printf("Nie znaleziono komentarza\n");
-        return 0;
+        return plik;
     }
     else if (c == EOF)
     {
         if (_DEBUG) printf("EOF\n");
-        return 2;
+        return NULL;
     }
     else
     {
-        printf("Błąd odczyt z pliku\n");
-        return 1;
+        printf("Błąd odczytu z pliku\n");
+        return NULL;
     }
 }
--- plik.h
+++ plik.h
@@ -6,9 +6,12 @@
 #include <stdio.h>
 
 #define MAX_FILE_NAME 20
+#define FILE_OPEN_ERR 1
+#define FILE_READ_ERR 2
 
 int odczytaj_plik(element *lista);
-int odczytaj_rodzaj_obrazka(FILE *plik, obraz *img);
-int sprawdz_czy_komentarz(FILE *plik);
+FILE * odczytaj_rodzaj_obrazka(FILE *plik, obraz *img);
+FILE * odczytaj_wielkosc_obrazka(FILE *plik, obraz *img);
+FILE * sprawdz_czy_komentarz(FILE *plik);
 
 #endif // PLIK_H
