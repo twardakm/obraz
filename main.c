661d5d6b008ae29be10f8d1cfd599165cf074e5f
twardakm <twardakm@gmail.com>
2013-11-24 01:07:40 +0100

    Poprawione zwalnianie pamięci

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "lista.h"
+#include "menu.h"
 
 int main(void)
 {
     element *lista = NULL;
 
     ustaw_kodowanie();
-    wiadomosc_powitalna(lista);
+    lista = wiadomosc_powitalna(lista);
 
     lista = clear(lista);
     return 0;
